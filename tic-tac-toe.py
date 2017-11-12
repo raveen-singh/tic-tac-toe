7a1728877b837f2777e67f939247dc347452161f
raveensingh123 <073112187@gapps.yrdsb.ca>
2017-11-11 20:40:37 -0500

    Refined Input

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -2,54 +2,72 @@
 from IPython.display import clear_output
 import random
 
-board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 win = [[1, 2, 3],[4, 5, 6],[7, 8, 9],[1, 4, 7],[2, 5, 8],[3, 6, 9],[1, 5, 9],[3, 5, 7]]
 
+def names():
+    print("What is your name?")
+    global name1
+    name1 = input()
+    print("What is your name?")
+    global name2
+    name2 = input()
 
 def print_board():
-    print(board[0], board[1], board[2])
-    print(board[3], board[4], board[5])
-    print(board[6], board[7], board[8])
-
+    print(board[0])
+    print(board[1])
+    print(board[2])
 
 def order_of_play():
-    players = ["Player 1", "Player 2"]
+    players = [name1, name2]
     global rand
     rand = random.choice(players)
     print("The player going first is", rand)
     global opp_rand
-    if rand == "Player 1":
-        opp_rand = "Player 2"
+    if rand == name1:
+        opp_rand = name2
     else:
-        opp_rand = "Player 1"
+        opp_rand = name1
 
 def x_or_o():
     print(rand,"do you want to be X or O?")
-    marker = input()
-    if marker == "X" or "x":
-        print(rand, "is now X")
-        print(opp_rand, "is now O")
+    global marker
+    marker = input(str())
+    print(marker)
+    if marker == "X" or marker == "x":
+        global opp_marker
+        opp_marker = "O"
+        print(rand, "is X")
+        print(opp_rand, "is O")
+    elif marker == "O" or marker =="o":
+        opp_marker = "X"
+        print(rand, "is O")
+        print(opp_rand, "is X")
 
 
-def player_one(row, col):
-    print(rand, "What is your first move?")
-    print(col)
-    print(row)
-    row = (input("What row do you want to go in?"))
-    col = (input("What col do you want to go?"))
+def player_one():
+    print(rand, "what row do you want to go in?")
+    row = int(input())
+    print(rand, "what col do you want to go?")
+    col = int(input())
+    board[row - 1][col -1] = marker
+
+def player_two():
+    print(opp_rand, "what row do you want to go in?")
+    row = int(input())
+    print(opp_rand, "what col do you want to go?")
+    col = int(input())
+    board[row - 1][col -1] = opp_marker
 
 
 
-def player_two(row1, col1):
-    print(opp_rand, "What is your move")
-    print(col1)
-    print(row1)
 
 
-#row1 = (input("What row do you want to go in?"))
-#col1 = (input("What col do you want to go?"))
-
-print_board()
+names()
 order_of_play()
 x_or_o()
-player_one(row, col)
+print_board()
+player_one()
+print_board()
+player_two()
+print_board()
